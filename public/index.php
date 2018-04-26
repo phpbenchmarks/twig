1285c8234ef86ab3697d77dc18424ba11ad58ed7
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-04-26 16:52:48 +0200

    Use new repository structure

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 require_once '../vendor/autoload.php';
 
 $cacheDir = '../var/cache';
-require_once '../vendor/phpbenchmarks/twig/helloworld.php';
+require_once '../vendor/phpbenchmarks/twig-common/helloworld.php';
 
 // require phpbenchmarks stats.php here when needed
