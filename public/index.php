b740ac94e8f1b08a2ad569de86d8fb690c8ecaef
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-05-30 16:50:15 +0200

    Create Hello World benchmark

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+$cacheDir = '../var/cache';
+require_once '../vendor/phpbenchmarks/twig-common/helloworld.php';
+
+// require phpbenchmarks stats.php here when needed
