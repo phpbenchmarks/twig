2e2ea7a170464e059fb9d73b62e5753edc36285c
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-12-18 23:44:47 +0000

    Add stats

--- public/index.php
+++ public/index.php
@@ -4,3 +4,5 @@ require_once '../vendor/autoload.php';
 
 $cacheDir = '../var/cache';
 require_once '../vendor/phpbenchmarks/twig-common/helloworld.php';
+
+// require phpbenchmarks stats.php here when needed
